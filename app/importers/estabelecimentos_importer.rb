70034f9987e50b8650f5b83244d178b787b25e67
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2022-07-25 19:59:52 -0300

    Cria entitdade para cidade

--- app/importers/estabelecimentos_importer.rb
+++ app/importers/estabelecimentos_importer.rb
@@ -1,15 +1,15 @@
 class EstabelecimentosImporter < ApiImporter
   def import
-    Estados.new.all.each do |estado|
-      import_estado(estado[:id])
+    Estados.new.each do |estado|
+      import_estado(estado)
     end
   end
 
   private
 
-  def import_estado(id)
-    Cidades.new(estado_id: id).all.each do |cidade|
-      import_cidade(cidade[:id])
+  def import_estado(estado)
+    estado.cidades.each do |cidade|
+      import_cidade(cidade.id)
     end
   end
 
