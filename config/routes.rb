992ad2752a4e5c9c8cb2fb461a2d9c25cd05bc46
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2022-07-25 18:33:30 -0300

    Clenaup

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root "profissionais#index"
-  get '/test', to: "profissionais#test"
   get '/estados', to: "wizzard#estados"
   get '/cidades', to: "wizzard#cidades", as: :cidades
   get '/estabelecimentos', to: "wizzard#estabelecimentos", as: :estabelecimentos
